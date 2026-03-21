cb55bae4b6ca3a6f8df3b1dcb2d6d311119a852e
IbnuZaidan43 <akbaribnu031@gmail.com>
2026-03-21 22:28:48 +0700

    justify design

--- jagawara-arenta/src/components/weatherPrediction.js
+++ jagawara-arenta/src/components/weatherPrediction.js
@@ -1,5 +1,7 @@
 'use client';
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import React from 'react';
 
 export default function Weather(){
     const [weatherData, setWeatherData] = useState(null);
@@ -12,7 +14,7 @@ export default function Weather(){
     useEffect(() => {
         const fetchWeather = async () => {
             try {
-                const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto`);
+                const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`);
                 const data = await res.json();
                 setWeatherData(data);
 
@@ -33,44 +35,90 @@ export default function Weather(){
         const hourlyTimes = weatherData.hourly.time;
         const hourlyTemp = weatherData.hourly.temperature_2m;
         const hourlyHumidity = weatherData.hourly.relativehumidity_2m;
+        const hourlyCode = weatherData.hourly.weather_code;
     
         return hourlyTimes.map((time, index) => ({
             time: time,
             temp: hourlyTemp[index],
-            humidity: hourlyHumidity[index]
+            humidity: hourlyHumidity[index],
+            weather_code: hourlyCode[index]
         })).filter(item => item.time.startsWith(selectedDate));
     };
-
+    
     const selectedHourlyData = getHourlyData();
+    function getWeatherCategory(code) {
+        if (code >= 0 && code <= 10) return "Cerah";
+        if (code >= 11 && code <= 50) return "Berawan";
+        if (code >= 51 && code <= 62) return "Hujan Ringan";
+        if (code >= 63 && code <= 72) return "Hujan Sedang";
+        if (code >= 73 && code <= 83) return "Hujan deras";
+        if (code >= 84) return "Badai Petir";
+        return "Tidak diketahui";
+    }
+
+    function getWeatherIcon(code) {
+        if (code >= 0 && code <= 10) return "/icon/Sun.svg";
+        if (code >= 11 && code <= 49) return "/icon/Cloud.svg";
+        if (code >= 50 && code <= 80) return "/icon/CloudRain.svg";
+        if (code >= 81) return "/icon/CloudLightning.svg";
+        return "Tidak diketahui";
+    }
 
     return (
-        <div className="p-5 max-w-4xl mx-auto">
-            <h2 className="text-2xl font-bold mb-4">Prediksi Cuaca Desa Cipelah</h2>
-            <div className="flex gap-4 overflow-x-auto pb-4">
+        <div className="p-5 max-w-[1290px] bg-white mx-auto border-3 border-[#936440]/60">
+            <h2 className="text-[#0B592F] text-[40px] leading-tight font-bold">Prediksi Cuaca Desa Cipelah</h2>
+            <h3 className="text-[#936440] text-[20px] leading-tight font-bold mb-4 ">Click Hari untuk Detail Jam</h3>
+            <div className="flex gap-4 -mx-5 px-4 border-y-3 border-[#936440]/60 py-4 overflow-hidden">
                 {weatherData.daily.time.map((date, index) => {
                     const maxTemp = weatherData.daily.temperature_2m_max[index];
                     const minTemp = weatherData.daily.temperature_2m_min[index];
+                    const dailyCode = weatherData.daily.weather_code[index];
+                    const weatherCategory = getWeatherCategory(dailyCode);
+                    const iconPath = getWeatherIcon(dailyCode);
                     const isSelected = selectedDate === date;
+                    const isLastItem = index === weatherData.daily.time.length - 1;
 
                     return (
-                        <div key={date} onClick={() => setSelectedDate(date)} className={"min-w-[120px] p-4 rounded-lg cursor-pointer transition-all border " + (isSelected ? "bg-blue-600 text-white border-blue-600 shadow-lg scale-105" : "bg-white text-gray-800 border-gray-200 hover:bg-gray-50")}>
-                            <p className="text-sm font-semibold">{new Date(date).toLocaleDateString('id-ID', { weekday: 'short', day: 'numeric', month: 'short'})}</p>
-                            <p className="text-lg font-bold mt-2">{maxTemp}°C</p>
-                            <p className="text-xs opacity-80">{minTemp}°C</p>
-                        </div>
+                        <React.Fragment key={date}>
+                            <div key={date} onClick={() => setSelectedDate(date)} className={"flex flex-col min-w-[100px] p-11 -m-4 cursor-pointer transition-all text-center items-center " + (isSelected ? "bg-[#0B592F]/15 hover:bg-[#F0E7D7]" : "bg-white hover:bg-gray-50")}>
+                                <p className="text-black text-2xl font-bold">{new Date(date).toLocaleDateString('id-ID', { weekday: 'long'})}</p>
+                                <p className="text-[#936440] text-sm font-semibold">{new Date(date).toLocaleDateString('id-ID', { day: 'numeric', month: 'short'})}</p>
+                                <div className="m-4 w-15 h-15 relative items-center">
+                                    <Image
+                                        src={iconPath} alt={weatherCategory}
+                                        fill className="object-contain"
+                                        />
+                                </div>
+                                <p className="text-[#936440] text-lg font-bold">{weatherCategory}</p>
+                                <p className="text-black text-lg font-semibold">{maxTemp}°C</p>
+                                <p className="text-[#936440] text-xs opacity-80">{minTemp}°C</p>
+                            </div>
+                            {!isLastItem && (
+                                <div className="w-[2px] -my-[17px] bg-[#936440]/60 shrink-0 rounded-full"></div>
+                            )}
+                        </React.Fragment>
                     );
                 })}
             </div>
-            <div className="mt-8 bg-gray-80 p-5 rounded-xl border border-gray-200">
-                <h3 className="text-lg font-bold mb-4 text-gray-800">Rincian per jam ({new Date(selectedDate).toLocaleDateString('id-ID', { weekday: 'short', day: 'numeric', month: 'long'})})</h3>
-                <div className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-8 gap-3">
+            <div className="p-5">
+                <h3 className="text-[#0B592F] text-[28px] font-bold mb-4">Cuaca {new Date(selectedDate).toLocaleDateString('id-ID', { weekday: 'long'})} {new Date(selectedDate).toLocaleDateString('id-ID', {day: 'numeric', month: 'long'})} per Jam</h3>
+                <div className="flex gap-3 overflow-x-auto costum-scrollbar pb-5">
                     {selectedHourlyData.map((data, index) => {
+                    // {selectedHourlyData.filter((_, index) => index % 2 === 0).map((data, index) => {
                         const hour = data.time.split('T')[1];
+                        const hourlyCode = data.weather_code;
+                        const iconPath = getWeatherIcon(hourlyCode);
                         return (
-                            <div key={index} className="bg-white p-3 rounded shadow-sm text-center border border-gray-100">
-                                <p className="text-sm font-semibold text-gray-600">{hour}</p>
-                                <p className="text-md font-bold text-blue-600 my-1">{data.temp}°C</p>
-                                <p className="text-xs text-gray-500">💧{data.humidity}%</p>
+                            <div key={index} className="bg-[#EAE2D5] p-5 rounded-xl shadow-lg text-center">
+                                <p className="text-lg font-semibold text-gray-600">{hour}</p>
+                                <div className="m-4 w-13 h-13 relative items-center">
+                                    <Image
+                                        src={iconPath} alt="weather category"
+                                        fill className="object-contain"
+                                        />
+                                </div>
+                                <p className="text-md font-bold text-black my-1">{data.temp}°C</p>
+                                <p className="text-xs text-blue-600">💧{data.humidity}%</p>
                             </div>
                         );
                     })}
